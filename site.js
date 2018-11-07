69ea50241e0ad656222ad8247528dad7915f09ec
Karl Stolley <karl.stolley@gmail.com>
2018-11-07 17:57:24 -0600

    Add skeletal JavaScript that adds a .js class

---
+++ site.js
@@ -0,0 +1,25 @@
+(function(){
+  // Check to make sure essential features are supported
+  if (typeof document.querySelector === 'undefined') {
+    // No? Then bye!!!
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', function(){
+    // Diagnostic...make sure DOMContentLoaded has fired...
+    console.log('OMG the DOM has loaded!');
+
+    // Grab the HTML element:
+    var html_tag = document.querySelector('html');
+
+    // Go and remove the .nojs class from <html>...
+    html_tag.classList.remove('nojs');
+
+    // And then add the .js class to <html>:
+    html_tag.classList.add('js');
+
+    // Otherwise, let's party
+  });
+
+  console.log("Hello from JavaScript! You have querySelector!");
+})();
